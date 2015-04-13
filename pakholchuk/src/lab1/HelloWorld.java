5659d48966d764b0cf09a2607e53d843331ce270
AntonPakholchuk <mrp.briar@gmail.com>
2015-04-13 12:26:07 +0300

    first java commit

---
+++ pakholchuk/src/lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package lab1;
+
+/**
+ * Created by Anton_Pakholchuk on 4/13/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
